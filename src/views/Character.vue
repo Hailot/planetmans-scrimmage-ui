0005dbf9ee0ba638232aae03a13e1e70c659f03e
Jesper Carlsen <jesper.bach.carlsen@bridgeit.dk>
2021-06-17 21:51:19 +0200

    player stuff

---
+++ src/views/Character.vue
@@ -0,0 +1,15 @@
+<template>
+<player-profile></player-profile>
+</template>
+
+<script>
+import PlayerProfile from "@/components/PlayerProfile";
+export default {
+  name: "Character",
+  components: {PlayerProfile}
+}
+</script>
+
+<style scoped>
+
+</style>
