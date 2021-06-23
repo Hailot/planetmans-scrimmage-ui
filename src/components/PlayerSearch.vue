70d8de2144172d3042ae5f38295d5093f149ff86
Jesper Carlsen <jesper.bach.carlsen@bridgeit.dk>
2021-06-24 01:55:52 +0200

    crude styling player

--- src/components/PlayerSearch.vue
+++ src/components/PlayerSearch.vue
@@ -1,52 +1,70 @@
 <template>
-  <v-container class="">
-    <h2 class="text-center">Player</h2>
+  <v-container class="justify-center">
+    <h2 class="text-center">SCORES</h2>
     <v-row no-gutters>
-      <v-form
-          ref="form"
-          v-model="valid"
-          lazy-validation
+      <v-col
+          cols="6"
+          sm="6"
+          md="6"
+          class="justify-center offset-3"
+
       >
-        <v-text-field
-            v-model="name"
-            label="Name"
-            required
-        ></v-text-field>
+        <v-card class="mb-6">
+          <h2 class="text-center">Player</h2>
+
+          <v-form
+              ref="form"
+              v-model="valid"
+              lazy-validation
+          >
+            <v-text-field
+                v-model="name"
+                label="Name"
+                required
+            ></v-text-field>
 
 
 
 
 
-        <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="submit"
-        >
-          GetData
-        </v-btn>
+            <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="submit"
+            >
+              GetData
+            </v-btn>
 
 
-      </v-form>
+          </v-form>
+        </v-card>
+      </v-col>
+      <v-col
+          cols="12"
+          sm="12"
+          md="12"
+          class="mt-10"
 
-    </v-row>
-    <v-row>
-      <v-data-table
-          :headers="columns"
-          :items="playerStats"
-          item-key="ScrimMatchId"
-          class="elevation-1" >
-        <template v-slot:item="{item}">
-          <tr >
-            <td v-for="(stat) in item" :key="stat.ScrimMatchId">
-             {{ stat }}
-            </td>
-          </tr>
-        </template>
-        <template v-slot:header.name="{ header }">
-          {{ header }}
-        </template>
-      </v-data-table>
+      >
+        <v-data-table
+            :headers="columns"
+            :items="playerStats"
+            item-key="ScrimMatchId"
+            class="elevation-1" >
+          <template v-slot:item="{item}">
+            <tr >
+              <td v-for="(stat) in item" :key="stat.ScrimMatchId">
+                {{ stat }}
+              </td>
+            </tr>
+          </template>
+          <template v-slot:header.name="{ header }">
+            {{ header }}
+          </template>
+        </v-data-table>
+
+      </v-col>
     </v-row>
   </v-container>
 </template>
